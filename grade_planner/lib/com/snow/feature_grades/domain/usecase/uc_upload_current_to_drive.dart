274f02cf264faa787184f0a107a1a0fdc1a0af0d
TrainerSnow <simon.bela.bumiller@gmx.de>
2022-12-17 14:39:36 +0100

    Refactor usecase

--- grade_planner/lib/com/snow/feature_grades/domain/usecase/uc_upload_current_to_drive.dart
+++ grade_planner/lib/com/snow/feature_grades/domain/usecase/uc_upload_current_to_drive.dart
@@ -7,40 +7,59 @@ import 'package:sprintf/sprintf.dart';
 import '../../data/networking/GoogleAuthClient.dart';
 
 const FILE_NAME = "grade_data_%s_%s_%s_.json";
+const LOGS_FILENAME = "logs.txt";
 
 class UploadCurrentToDrive {
   io.File currentFile;
 
   UploadCurrentToDrive(this.currentFile);
 
-  Future<google.File> call(GoogleSignInAccount account) async {
+  Future<google.File> call({required GoogleSignInAccount account, io.File? file}) async {
+    if (file == null) {
+      return _uploadFile(
+        account,
+        currentFile,
+        sprintf(FILE_NAME, [
+          DateTime.now().year,
+          DateTime.now().month,
+          DateTime.now().day,
+        ]),
+        (api) => _deleteOtherGradeFiles(api),
+      );
+    } else {
+      return _uploadFile(
+        account,
+        file,
+        LOGS_FILENAME,
+        (api) => _deleteOtherLogFiles(api),
+      );
+    }
+  }
+
+  Future<google.File> _uploadFile(GoogleSignInAccount account, io.File file, String targetName, Function(google.DriveApi) onDelete) async {
     final headers = await account.authHeaders;
     final authClient = GoogleAuthClient(headers);
     final driveApi = google.DriveApi(authClient);
 
-    _deleteOtherGradeFiles(driveApi);
+    onDelete(driveApi);
 
-    final Stream<List<int>> mediaStream = currentFile.openRead();
+    final Stream<List<int>> mediaStream = file.openRead();
     final list = await mediaStream.first;
 
     final media = google.Media(Future.value(list).asStream().asBroadcastStream(), list.length);
 
     final driveFile = google.File();
-    driveFile.name = sprintf(FILE_NAME, [
-      DateTime.now().year,
-      DateTime.now().month,
-      DateTime.now().day,
-    ]);
+    driveFile.name = targetName;
     return driveApi.files.create(driveFile, uploadMedia: media);
   }
 
-  void _deleteOtherGradeFiles(google.DriveApi driveApi) async {
+  void _deleteFilesWithCondition(bool Function(String) check, google.DriveApi driveApi) async {
     final files = await driveApi.files.list();
 
     if (files.files != null) {
       for (google.File file in files.files!) {
         if (file.name != null) {
-          if (file.name!.startsWith("grade_data") && file.name!.endsWith(".json")) {
+          if (check(file.name!)) {
             if (file.id != null) {
               driveApi.files.delete(file.id!);
             }
@@ -48,5 +67,13 @@ class UploadCurrentToDrive {
         }
       }
     }
+  }
+
+  void _deleteOtherLogFiles(google.DriveApi driveApi) async {
+    _deleteFilesWithCondition((name) => name == "logs.txt", driveApi);
+  }
+
+  void _deleteOtherGradeFiles(google.DriveApi driveApi) async {
+    _deleteFilesWithCondition((name) => name.startsWith("grade_data") && name.endsWith(".json"), driveApi);
   }
 }
